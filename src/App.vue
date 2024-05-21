a81c13609050e978adeffa111fb417576acf0892
Tasneem Wahdan <tasneem.90.wahdan@gmail.com>
2024-05-21 12:48:03 +0300

    lesson 36 lifecycle hooks https://www.youtube.com/watch?v=bc6czIBLKTg&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=6

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Ninja Reaction Timer</h1>
+ <button @click="start" :disabled="isPlaying">play</button>
+ <BlockToBeClicked v-if="isPlaying" :delay="delay" @endGame="endGame"/>
+ 
+ <ClickTimerResult v-if="showResults" :score="score" />
+
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlockToBeClicked from './components/BlockToBeClicked.vue'
+import ClickTimerResult from './components/ClickTimerResult.vue';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { BlockToBeClicked, ClickTimerResult },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: 0,
+      showResults: false,
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 1000 // between 2 and 7 seconds
+      this.showResults = false
+      //console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.showResults = true
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
